205b82b609fab79812e917e316b9d2ee04d4b9c2
anelpopaja <bounsy80@gmail.com>
2022-03-01 17:57:19 +0100

    adjusted instance values

--- Greedy Algorithm/CreateInstances.py
+++ Greedy Algorithm/CreateInstances.py
@@ -4,20 +4,20 @@ path = "C:\\Users\\PC\\Desktop\\OI projekat\\PythonFirstFitD\\"
 for i in range(15):
     f = open(path + "instance1\\instance" + str(i) + ".txt", "w+")
     f.write("100\n")
-    f.write("10000\n")
-    for j in range(10000):
+    f.write("5000\n")
+    for j in range(5000):
         f.write(str(random.randint(1, 100)) + "\n")
 
 for i in range(15):
     f = open(path + "instance2\\instance" + str(i) + ".txt", "w+")
     f.write("100\n")
-    f.write("500000\n")
-    for j in range(500000):
+    f.write("15000\n")
+    for j in range(15000):
         f.write(str(random.randint(1, 100)) + "\n")
 
 for i in range(15):
     f = open(path + "instance3\\instance" + str(i) + ".txt", "w+")
     f.write("100\n")
-    f.write("1000000\n")
-    for j in range(1000000):
+    f.write("45000\n")
+    for j in range(45000):
         f.write(str(random.randint(1, 100)) + "\n")

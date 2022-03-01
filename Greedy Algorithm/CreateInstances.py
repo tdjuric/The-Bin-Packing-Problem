ba31e1fd3e26d7cd69186bb80076b0b7f411c100
anelpopaja <bounsy80@gmail.com>
2022-03-01 17:50:45 +0100

    Overall changes to the main algortihm

--- Greedy Algorithm/CreateInstances.py
+++ Greedy Algorithm/CreateInstances.py
@@ -1,23 +1,23 @@
 import random
 
-path = "C:\\Users\\Tanja\\PycharmProjects\\The Bin Packing Problem\\"
+path = "C:\\Users\\PC\\Desktop\\OI projekat\\PythonFirstFitD\\"
 for i in range(15):
     f = open(path + "instance1\\instance" + str(i) + ".txt", "w+")
     f.write("100\n")
-    f.write("5000\n")
-    for j in range(5000):
+    f.write("10000\n")
+    for j in range(10000):
         f.write(str(random.randint(1, 100)) + "\n")
 
 for i in range(15):
     f = open(path + "instance2\\instance" + str(i) + ".txt", "w+")
     f.write("100\n")
-    f.write("25000\n")
-    for j in range(25000):
+    f.write("500000\n")
+    for j in range(500000):
         f.write(str(random.randint(1, 100)) + "\n")
 
 for i in range(15):
     f = open(path + "instance3\\instance" + str(i) + ".txt", "w+")
     f.write("100\n")
-    f.write("500000\n")
-    for j in range(500000):
+    f.write("1000000\n")
+    for j in range(1000000):
         f.write(str(random.randint(1, 100)) + "\n")
--- Greedy Algorithm/main.py
+++ Greedy Algorithm/main.py
@@ -1,8 +1,8 @@
 import firstFitDecreasing as firstFitDecreasing
 import timeit
 
-path = "C:\\Users\\Tanja\\PycharmProjects\\The Bin Packing Problem\\"
-bin_size, number_of_instances, dict = firstFitDecreasing.readInInstances(path + "instance3\instance0.txt")
+path = "C:\\Users\\PC\\Desktop\\OI projekat\\PythonFirstFitD\\"
+bin_size, number_of_instances, dict = firstFitDecreasing.readInInstances(path + "instance1\instance0.txt")
 print("Bin size: " + str(bin_size))
 print("Number of Instances: " + str(number_of_instances))
 #print("Instances: " + str(dict))
@@ -14,7 +14,7 @@ def f1(dict,bin_size):
     firstFitDecreasing.first_fit_decreasing_algorithm(dict, bin_size, return_sizes=False)
 
 for i in range (1):
-    bin_size, number_of_instances, dict = firstFitDecreasing.readInInstances(path + "instance3\instance" +str(i)+ ".txt")
+    bin_size, number_of_instances, dict = firstFitDecreasing.readInInstances(path + "instance1\instance" +str(i)+ ".txt")
     print(len(firstFitDecreasing.first_fit_decreasing_algorithm(dict, bin_size, return_sizes=True)))
     t = timeit.Timer(lambda: f1(dict, bin_size))
     print(t.timeit(number=1))

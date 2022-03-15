654160478467038dfa2dd0883c36d2d7be551053
anelpopaja <bounsy80@gmail.com>
2022-03-15 19:26:04 +0100

    Added aditional functions to Genetic algorithm

--- Genetic/Bin.py
+++ Genetic/Bin.py
@@ -1,15 +1,29 @@
 #This needs to be changed
 class Bin:
-    def __init__(self, id, capacity, contents):
+    def __init__(self, id, capacity, fill):
         self.id = id
         self.capacity = capacity
-        self.contents = contents
+        self.fill = fill
+        self.contents = []
 
     def getId(self):
         return self.id
+
+    def getFill(self):
+        return self.fill
+
+    def setFill(self, fill):
+        self.fill = fill
 
     def getCapacity(self):
         return self.capacity
 
     def getContents(self):
         return self.contents
+
+    def addElement(self, element):
+        self.contents.append(element)
+
+    def __repr__(self):
+        s = "Id: " + str(self.id) + ", Capacity: " + str(self.capacity) + ", Contents: " + str(self.contents)
+        return s
--- Genetic/Chromosome.py
+++ Genetic/Chromosome.py
@@ -1,78 +1,103 @@
 import random
+from The_Bin_Packing_Problem.Genetic.Bin import Bin
+
+
 class Chromosome:
 
     # Initialization
+    # capacity, list of bins, max_bins, *chromosome
     def __init__(self, *args):
         self.capacity = args[0]
-        self.quota = args[1]
-        self.bins = args[2]
+        self.bins = args[1]
+        self.max_bins = args[2]
+        self.items = args[3]
         self.chromosome = []
-        if len(args) == 3:
-            print("hi")
-            #fillChromosome()
-            #setChromosome()
+        self.fitness = 0
+        if len(args) == 4:
+            print("Not a child chromosome")
+            self.setChromosome()
+            self.fillBins()
+            self.setFitness()
 
         # Used for intitializing a child chromosome
         else:
-            self.chromosome = args[3]
+            self.chromosome = args[4]
             #setFitness()
 
-    '''
-    def __init__(self, capacity, quota, bins, chromosome):
-        self.capacity = capacity
-        self.quota = quota
-        self.bins = bins
-        self.chromosome = chromosome
-        #setFitness()
-    '''
-
-    # Fills the chromosome with 0
-    def fillChromosome(self):
-        for i in range(len(self.bins)):
-            self.chromosome.append(0)
 
     def getChromosome(self):
         return self.chromosome
 
-    # Randomly populating chromosome List with 1 and 0
+    # Fills the chromosome with 0
+    def fillChromosome(self):
+        for i in range(self.max_bins):
+            self.chromosome.append(0)
+
+
+
+
+    # Randomly populating chromosome List with the index of a bin they will be placed in
     def setChromosome(self):
-        n = self.randomNumberOfBoxes()
-        while (n==0):
-            n = self.randomNumberOfBoxes()
+        n = self.max_bins
 
         for i in range(n):
-            self.chromosome.insert(i, 1)
+            place = random.randint(1, self.max_bins)
+            self.chromosome.insert(i, place)
         random.shuffle(self.chromosome)
-        self.setFitness
 
-    def randomNumberOfBoxes(self):
-        return random.randint(len(self.bins)+1)
+    # Filling bins with items according to the random chromosome list
+    def fillBins(self):
+        for i, el in enumerate (self.chromosome):
+            self.bins[el-1].addElement(items[i])
+            self.bins[el-1].setFill((self.bins[el-1].getFill()) + (items[i]))
 
 
     #Fitness function
     def setFitness(self):
         k = 2
-        n = len(self.bins)
+        n = 0
+        for bin in self.bins:
+            if (len(bin.contents) != 0):
+                n+=1
+
         sum = 0
-        for i, el in enumerate (self.bins):
+        for i, el in enumerate(self.bins):
             fill = el.getFill()
             capacity = el.getCapacity()
 
-            value = (fill / capacity) ^ k
+            value = (fill / capacity) ** 5
             sum += value
-
-        return sum / n
+        print(n)
+        self.fitness = sum / n
 
 
 
     def __repr__(self):
-        s = "Capacity: " + str(self.capacity) + "\nQuota: " + str(self.quota) + "\nBins: " + str(self.bins) + "\nChromosome list: " + str(self.chromosome)
+        s = "Capacity: " + str(self.capacity) + "\nBins: " + str(self.bins) + "\nChromosome list: " + str(self.chromosome) + "\n Fitness: " + str(self.fitness)
         return s
 
-a = Chromosome(5,5,[1,2,3,4,5,6], [])
+
+# TODO Napisati funkcija za generisanje praznih binova
+
+b1 = Bin(1, 5, 0)
+b2 = Bin(2, 5, 0)
+b3 = Bin(3, 5, 0)
+b4 = Bin(4, 5, 0)
+b5 = Bin(5, 5, 0)
+
+# Ovo cemo citati iz filea
+items = [5,1,3,2,4]
+
+a = Chromosome(5, [b1, b2, b3, b4, b5], 5, items)
+
+
 print(a)
-a.fillChromosome()
-print(a)
+# Random redoslijed popunjavanja kanti
+# a.setChromosome()
+# Popunjavanje kanti po random redoslijedu
+# a.fillBoxes()
+# Odredivanje fitness funkcija chromosoma
+# a.setFitness()
 
 
 
